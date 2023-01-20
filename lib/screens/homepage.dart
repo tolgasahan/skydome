efc036db2d975c408196a7a745174deb81177caf
Tolga <sahantolga1021567@gmail.com>
2023-01-20 17:18:28 +0300

    commit4 UI almost done. Code needs refactoring.

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,10 +1,13 @@
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:skydome/data/chart-data.dart';
 import 'package:skydome/decorations/backgrounds/background-decorator.dart';
 import 'package:skydome/decorations/decorations.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:skydome/widgets/profit.dart';
 import 'package:page_view_dot_indicator/page_view_dot_indicator.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,6 +17,23 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<ChartData> chartData = <ChartData>[
+    ChartData(1, 21),
+    ChartData(2, 24),
+    ChartData(3, 35),
+    ChartData(4, 38),
+    ChartData(5, 54),
+    ChartData(6, 21),
+  ];
+  final List<ChartData> chartData2 = <ChartData>[
+    ChartData(1, 28),
+    ChartData(2, 20),
+    ChartData(3, 15),
+    ChartData(4, 58),
+    ChartData(5, 44),
+    ChartData(6, 20),
+  ];
+  late int pageCount = 3;
   late int selectedPage;
   late final PageController _bottomPageController;
   String _appBarText = "Fabirka Genel Verimliliği";
@@ -28,6 +48,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -51,11 +72,10 @@ class _HomePageState extends State<HomePage> {
         children: [
           Column(
             children: [
-              Align(
-                alignment: Alignment.topCenter,
+              Expanded(
+                flex: 27,
                 child: Container(
-                    height: MediaQuery.of(context).size.height * 0.475,
-                    width: double.infinity,
+                    alignment: Alignment.topCenter,
                     color: Decorations.homePageColor,
                     child: CircularPercentIndicator(
                       radius: 180,
@@ -76,59 +96,128 @@ class _HomePageState extends State<HomePage> {
                       ),
                     )),
               ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.130,
-                width: double.infinity,
-                color: Decorations.homePageColor,
-                child: Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 2 - 1,
-                      child: Profit(Icons.check_circle, 53.72, "Kazanç"),
-                    ),
-                    Container(
-                      color: Colors.white,
-                      width: 2,
-                      height: 40,
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width / 2 - 1,
-                      child: Profit(Icons.radar_rounded, 64.32, "Optimum"),
-                    )
-                  ],
+              Expanded(
+                flex: 8,
+                child: Container(
+                  color: Decorations.homePageColor,
+                  child: Row(
+                    children: [
+                      Expanded(
+                        flex: 100,
+                        child: Profit(Icons.check_circle, 53.72, "Kazanç"),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.white,
+                          height: 35,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 100,
+                        child: Profit(Icons.radar_rounded, 64.32, "Optimum"),
+                      )
+                    ],
+                  ),
                 ),
               ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.270,
-                child: PageView(
-                  controller: _bottomPageController,
-                  onPageChanged: (index) {
-                    setState(() {
-                      selectedPage = index;
-                    });
-                  },
-                  children: [
-                    Center(
-                      child: Text('First Page'),
-                    ),
-                    Center(
-                      child: Text('Second Page'),
-                    ),
-                    Center(
-                      child: Text('Third Page'),
-                    ),
-                  ],
+              Expanded(
+                flex: 15,
+                child: Container(
+                  child: PageView.builder(
+                    itemCount: pageCount,
+                    controller: _bottomPageController,
+                    onPageChanged: (index) {
+                      setState(() {
+                        selectedPage = index;
+                      });
+                    },
+                    itemBuilder: (context, itemCount) {
+                      return Column(
+                        children: [
+                          Expanded(
+                              flex: 2,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                      child: Container(
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 15),
+                                    child: Text(
+                                        "Makina ${(itemCount + 1).toString()}"),
+                                  )),
+                                  Expanded(
+                                      child: Container(
+                                    alignment: Alignment.centerRight,
+                                    padding:
+                                        EdgeInsets.symmetric(horizontal: 10),
+                                    child: Text(
+                                        "Makina ${(itemCount + 1).toString()}"),
+                                  ))
+                                ],
+                              )),
+                          Expanded(
+                            flex: 10,
+                            child: Container(
+                              padding: EdgeInsets.only(right: 5),
+                              child: SfCartesianChart(
+                                legend: Legend(
+                                    itemPadding: 1,
+                                    isVisible: true,
+                                    offset: Offset(0, -60),
+                                ),
+                                plotAreaBorderWidth: 0,
+                                primaryYAxis: NumericAxis(
+                                  labelStyle: TextStyle(fontSize: 0),
+                                  axisLine: const AxisLine(width: 0),
+                                  majorTickLines:
+                                      const MajorTickLines(width: 0),
+                                  majorGridLines:
+                                      const MajorGridLines(width: 0),
+                                ),
+                                primaryXAxis: NumericAxis(
+                                  axisLine: const AxisLine(width: 0),
+                                  majorTickLines:
+                                      const MajorTickLines(width: 0),
+                                  majorGridLines:
+                                      const MajorGridLines(width: 0),
+                                ),
+                                series: <ChartSeries>[
+                                  SplineAreaSeries<ChartData, int>(
+                                      dataSource: chartData,
+                                      xValueMapper: (ChartData data, _) =>
+                                          data.x,
+                                      yValueMapper: (ChartData data, _) =>
+                                          data.y),
+                                  SplineAreaSeries<ChartData, int>(
+                                      dataSource: chartData2,
+                                      xValueMapper: (ChartData data, _) =>
+                                          data.x,
+                                      yValueMapper: (ChartData data, _) =>
+                                          data.y)
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  ),
                 ),
               ),
-              PageViewDotIndicator(
-                size: Size(MediaQuery.of(context).size.height * 0.008, MediaQuery.of(context).size.height * 0.008),
-                currentItem: selectedPage,
-                count: 3,
-                unselectedColor: Colors.black26,
-                selectedColor: Colors.black,
-                duration: Duration(milliseconds: 200),
-                boxShape: BoxShape.circle,
-              ),
+              Expanded(
+                flex: 2,
+                child: PageViewDotIndicator(
+                  unselectedSize: Size(8, 8),
+                  size: Size(8, 8),
+                  currentItem: selectedPage,
+                  count: pageCount,
+                  unselectedColor: Colors.black26,
+                  selectedColor: Colors.black,
+                  duration: Duration(milliseconds: 200),
+                  boxShape: BoxShape.circle,
+                ),
+              )
             ],
           ),
           Container(
@@ -145,6 +234,7 @@ class _HomePageState extends State<HomePage> {
                           padding: EdgeInsets.all(5),
                           child: Text("Makina " + (item + 1).toString())),
                     ),
+                    // Cardları böl
                   );
                 }),
           )
